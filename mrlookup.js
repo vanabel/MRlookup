85b6c9e073babab6726a26bebacb983b3acc95a8
vanabel <van141.abel@gmail.com>
2025-03-08 14:03:55 +0800

    work version with basic functions

--- mrlookup.js
+++ mrlookup.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @namespace         vanabeljs
 // @name              MRLookup
+// @namespace         vanabeljs
 // @description       Extract BibTeX data automatically and modify BibTeX Key to AUTHOR_YEAR_TITLE.
 // @description:ZH-CN 自动提取BibTeX数据并修改BibTeX关键字为AUTHOR_YEAR_TITLE的形式.
 // @copyright         2018, Van Abel (https://home.vanabel.cn)
@@ -46,32 +46,37 @@ function IgnoreStringToRegExp(arr) {
 /*split bibdata*/
 
 function parseBibTexLine(text) {
-	var m = text.match(/^\s*(\S+)\s*=\s*/);
-	if (!m) {
-		console.log('line: "' + text + '"');
-		throw new Error('Unrecogonised line format');
-	}
-	var name = m[1];
-	var search = text.slice(m[0].length);
-	var re = /[\n\r,{}]/g;
-	var braceCount = 0;
-	var length = m[0].length;
-	do {
-		m = re.exec(search);
-		if (m[0] === '{') {
-			braceCount++;
-		} else if (m[0] === '}') {
-			if (braceCount === 0) {
-				throw new Error('Unexpected closing brace: "}"');
-			}
-			braceCount--;
+	try {
+		var m = text.match(/^\s*(\S+)\s*=\s*/);
+		if (!m) {
+			console.error('Invalid line format:', text);
+			return null;
 		}
-	} while (braceCount > 0);
-	return {
-		field: name,
-		value: search.slice(0, re.lastIndex),
-		length: length + re.lastIndex + m[0].length
-	};
+		var name = m[1];
+		var search = text.slice(m[0].length);
+		var re = /[\n\r,{}]/g;
+		var braceCount = 0;
+		var length = m[0].length;
+		do {
+			m = re.exec(search);
+			if (m[0] === '{') {
+				braceCount++;
+			} else if (m[0] === '}') {
+				if (braceCount === 0) {
+					throw new Error('Unexpected closing brace: "}"');
+				}
+				braceCount--;
+			}
+		} while (braceCount > 0);
+		return {
+			field: name,
+			value: search.slice(0, re.lastIndex),
+			length: length + re.lastIndex + m[0].length
+		};
+	} catch (error) {
+		console.error('Error parsing BibTeX line:', error);
+		return null;
+	}
 }
 function parseBibTex(text) {
 	var m = text.match(/^\s*@([^{]+){([^,\n]+)[,\n]/);
@@ -90,72 +95,105 @@ function parseBibTex(text) {
 	}
 	return result;
 }
-var els = document.getElementsByTagName('pre');
-for (var i = 0, l = els.length; i < l; i++) {
-	var el = els[i];
-	var bibdata = parseBibTex(el.innerHTML);
-	/*Extract author*/
-	//var aureg = new RegExp('(?:(\\w+),(?:[^,}](?!and))+)+', 'g');
-	//console.log(audata.match(aureg));
-	var audata = bibdata.AUTHOR;
 
-	var cleanreg = new RegExp('[{}\\\\\'"`]', 'g');
-	//console.log(cleanreg);
-	var au = "";
-	while( au!=audata){
-		au=audata;
-		audata=au.replace(cleanreg, '');
-	}
-	//console.log(au);
-	//clean author include \v+space and $\cmd$
-	audata = audata.replace(/\\[a-z]\s|(\$.*?\$)|(\\")/gi, '');
-	//console.log(audata);
-	//remove double "," in one name
-	audata = audata.replace(/(?:(?:and)?(\w+,\s*\w+\.?)(,.*?)?)+/g, '$1');
-	//console.log(audata);
-	//extract first name
-	au = au.replace(/(?:(\w+),(?:[^,}](?!and))+)+/g, '$1');
-	//remove and
-	au = au.replace(/\s*and\s*/g, '');
-	//console.log(au);
-	/*Extract Year*/
-	var year = bibdata.YEAR.replace(cleanreg, '');
-	//console.log(year);
-	/*Extract Title*/
-	var title = bibdata.TITLE;
-	//clean \cmd+space
-	title = title.replace(/\\\w+\s*/g, '');
-	var titleclean = new RegExp(IgnoreStringToRegExp(IgnoreStringInTitle), 'gi');
-	//we don't need to clean and, but we need to clean -,{,},: and ,
-	//var cleanreg = new RegExp('[{}:-]|,|(\\r\\s*)|(\\n\\s*)', 'g');
-	title = title.replace(/[\\\'\"{}:-]|,|(\r\s*)|(\n\s*)/g, '');
-	//var cleandollar = new RegExp('\\$.*?\\$', 'g');
-	title = title.replace(/\$.*?\$/g, '');
-	var titlefinal = title.replace(titleclean, '');
-	while (title != titlefinal) {
-		//recursively remove the ignore word
-		title = titlefinal;
-		titlefinal = title.replace(titleclean, '');
-	} //var titlereg = new RegExp('(\\w+)(\\s*\\w+)*', 'gi');
-
-	title = title.replace(/(\w+).*$/gi, '$1').replace(/\s*/g, '');
-	var bibkey = au + year + title;
-	//console.log(bibkey);
-	var bibkeyreg = new RegExp('MR\\d+', 'gi');
-	el.innerHTML = el.innerHTML.replace(bibkeyreg, bibkey);
-	/*click to copy to clipboard*/
-	el.addEventListener('click', function () {
-		//Actual new line break
-		//var linebreakreg = new RegExp('\\r|\\n', 'g');
-		var bibdata_lb = this.innerHTML.replace(/\r|\n/g, '\r\n').replace(/^\r\n/g, '').replace(/\s*$/g, '\r\n').replace(/\r\n\r\n/g, '\r\n');
-		GM_setClipboard(bibdata_lb);
+function cleanAuthorName(author) {
+	if (!author) return '';
+	
+	// 分割多个作者
+	let authors = author.split(/\s*and\s*/);
+	// 获取所有作者的姓
+	let lastNames = authors.map(author => {
+		// 提取姓（通常是逗号前的部分）
+		let lastName = author.split(',')[0];
+		// 清理特殊字符
+		return lastName.replace(/[{}\\\s\'"`]/g, '');
 	});
+	// 拼接所有作者的姓
+	return lastNames.join('');
+}
+
+function cleanTitle(title) {
+	if (!title) return '';
+	
+	// 移除LaTeX命令和数学符号
+	title = title.replace(/\\[a-zA-Z]+/g, '')          // 移除LaTeX命令
+		  .replace(/\$[^$]*\$/g, '')                   // 移除数学公式
+		  .replace(/[{}\\\'"`]/g, '')                  // 移除特殊字符
+		  .replace(/\{[^}]*\}/g, '');                  // 移除花括号内容
+	
+	// 按空格、连字符和标点符号分割成单词
+	let words = title.split(/[\s\-,.:;]+/)
+		.filter(word => word.length > 0)               // 移除空字符串
+		.map(word => word.replace(/[^a-zA-Z]/g, '')); // 只保留字母
+	
+	// 找到第一个有意义的单词
+	for (let word of words) {
+		// 转换为小写进行比较
+		let lowercaseWord = word.toLowerCase();
+		// 检查是否是忽略词或单个字母
+		if (!IgnoreStringInTitle.includes(lowercaseWord) && 
+			word.length > 1 && 
+			!/^\d+$/.test(word)) {  // 排除纯数字
+			// 转换为首字母大写
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+		}
+	}
+	
+	// 如果没有找到合适的单词，返回空字符串
+	return '';
+}
+
+var els = document.getElementsByTagName('pre');
+for (var i = 0; i < els.length; i++) {
+	try {
+		var el = els[i];
+		var bibdata = parseBibTex(el.innerHTML);
+		if (!bibdata) continue;
+
+		// 提取作者
+		var author = cleanAuthorName(bibdata.AUTHOR);
+		
+		// 提取年份 - 简化版本
+		var year = '';
+		if (bibdata.YEAR) {
+			// 匹配第一个四位数字
+			let yearMatch = bibdata.YEAR.match(/\d{4}/);
+			if (yearMatch) {
+				year = yearMatch[0];
+			}
+		}
+		
+		// 提取标题
+		var title = cleanTitle(bibdata.TITLE);
+		
+		// 组合BibTeX键
+		var bibkey = author + year + title;
+		
+		// 替换原有的MR号
+		el.innerHTML = el.innerHTML.replace(/MR\d+/g, bibkey);
+		
+		// 添加点击复制功能
+		el.addEventListener('click', function() {
+			try {
+				var bibdata_lb = this.innerHTML
+					.replace(/\r|\n/g, '\r\n')
+					.replace(/^\r\n/g, '')
+					.replace(/\s*$/g, '\r\n')
+					.replace(/\r\n\r\n/g, '\r\n');
+				GM_setClipboard(bibdata_lb);
+			} catch (error) {
+				console.error('Error copying to clipboard:', error);
+			}
+		});
+	} catch (error) {
+		console.error('Error processing BibTeX entry:', error);
+	}
 }
 
 /**
  * auto set bibtex item checked for mrlookup site
  */
-var url=location.pathname;
-if ("url:contains('mrlookup')") {
-	document.getElementsByName('format')[1].checked=1;
+var url = location.pathname;
+if (url.includes('mrlookup')) {
+	document.getElementsByName('format')[1].checked = true;
 }
